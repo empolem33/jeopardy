d9ba779249c7bec76ed0a88a690320770cf17169
empolem33 <inspiron0925@yahoo.com>
2022-06-18 19:32:07 -0700

    js fix

--- main.js
+++ main.js
@@ -1,18 +1,44 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+// document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  console.log(choice)
-  const url = ``
+// function getFetch(){
+//   const choice = document.querySelector('input').value
+//   console.log(choice)
+//   const url = ``
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)     
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+//   fetch(url)
+//       .then(res => res.json()) // parse response as JSON
+//       .then(data => {
+//         console.log(data)     
+//       })
+//       .catch(err => {
+//           console.log(`error ${err}`)
+//       });
+// }
+
+
+
+initBoard()
+
+function initBoard() {
+    let board = document.getElementById('clue-board')
+    //generate 5 rows, then place 6 boxes in each row 
+
+    for(let i=0; i < 5; i++) {
+        let row = document.createElement("div")
+        let boxValue = 200 * (i+1)
+        row.className = 'clue-row'
+
+        for(let j =0; j<6; j++){
+            let box = document.createElement('div')
+            box.className= 'clue-box'
+            box.textContent = "$" + boxValue
+            box.addEventListener('click',getClue,false)
+            row.appendChild(box)
+            function getClue(){
+                
+            }
+        }
+        board.appendChild(row)
+    }
 }
-
